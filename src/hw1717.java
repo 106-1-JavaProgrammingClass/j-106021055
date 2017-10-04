d98e22cda4d1b0ddaef983c58eb2017a47af1551
106021055 <32056108+106021055@users.noreply.github.com>
2017-10-04 17:45:27 +0800

    Add files via upload

---
+++ src/hw1717.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class hw1717 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        System.out.println((int)Math.sqrt(x)*10);
+    }
+}
