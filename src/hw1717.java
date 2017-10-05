05dd0129783fe7a494d40d5bbf34022ab85d083a
106021055 <32056108+106021055@users.noreply.github.com>
2017-10-05 22:16:33 +0800

    Delete hw1717.java

--- src/hw1717.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class hw1717 {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int x = scn.nextInt();
-        System.out.println((int)Math.sqrt(x)*10);
-    }
-}
