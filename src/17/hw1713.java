24e48a40478c568101b9886f287b60669379652c
106021055 <32056108+106021055@users.noreply.github.com>
2018-05-03 22:30:27 +0800

    Delete hw1713.java

--- src/17/hw1713.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class hw1713 {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        char ch = scn.next().charAt(0);
-        System.out.println((int)ch);
-    }
-}
